5e9e61f696d49b3209c790bd45647ebd2919984c
InKyeongBae <hwang0809@cau.ac.kr>
2022-01-19 13:44:00 +0900

    update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -16,10 +16,13 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	i = 0;
-	while (s[i] != '\0')
+	if (s != 0 && f != 0)
 	{
-		f(i, s + i);
-		i++;
+		i = 0;
+		while (s[i])
+		{
+			f(i, s + i);
+			i++;
+		}
 	}
 }
